0fb4248b61f3e3a52209f6ee5d3eefe72d30628f
Andreas Daxer <andreas_daxer@hotmail.com>
2017-03-31 16:30:25 +0200

    Added SignalR exchange per chat

--- BooksApp.Core/ViewModels/SearchListViewModel.cs
+++ BooksApp.Core/ViewModels/SearchListViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using BooksApp.Data;
 using BooksApp.Data.Interfaces;
+using BooksApp.Data.Services;
 using MvvmCross.Core.ViewModels;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +15,7 @@ namespace BooksApp.Core.ViewModels
     public class SearchListViewModel : MvxViewModel, ICanDeleteItem
     {
         private IBookStorage m_BookStorage;
+        private PortableChat m_Chat;
 
         public SearchListViewModel(IBookStorage bookStorage)
         {
@@ -24,17 +27,38 @@ namespace BooksApp.Core.ViewModels
         public bool IsLoading { get; set; } = true;
 
         public ICommand DeleteCommand => new MvxCommand<BookEntry>(DeleteItem);
+        public ICommand ShareCommand => new MvxCommand<BookEntry>(ShareItem);
+
+        private async void ShareItem(BookEntry entry)
+        {
+           await m_Chat.Send(JsonConvert.SerializeObject(entry));
+        }
 
         protected override void InitFromBundle(IMvxBundle parameters)
         {
             IsLoading = true;
             base.InitFromBundle(parameters);
+            InitChat();
             LoadBooks(); 
+        }
+
+        private async void InitChat()
+        {
+            m_Chat = new PortableChat();
+            if(await m_Chat.Connect("http://www.adaxer.de/signalr/hubs"))
+                m_Chat.Receive += OnReceiveShared; ;
+        }
+
+        private void OnReceiveShared(string json)
+        {
+            var entry = JsonConvert.DeserializeObject<BookEntry>(json);
+            if (!Books.Any(b => b.Id == entry.Id))
+                Books.Add(entry);
         }
 
         private async void LoadBooks()
         {
-            Books = new List<BookEntry>(await m_BookStorage.LoadBooksAsync());
+            Books = new MvxObservableCollection<BookEntry>(await m_BookStorage.LoadBooksAsync());
             IsLoading = false;
         }
 
